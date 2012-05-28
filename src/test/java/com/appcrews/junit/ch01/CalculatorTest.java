2def5a46f3b84b79d20885d633f8b4a14e707f46
f393907 <f393907@gmail.com>
2012-05-28 23:53:59 +0800

    稍微好一点的CalculatorTest程序

--- src/test/java/com/appcrews/junit/ch01/CalculatorTest.java
+++ src/test/java/com/appcrews/junit/ch01/CalculatorTest.java
@@ -7,9 +7,22 @@ public class CalculatorTest {
 		Calculator calculator = new Calculator();
 		double result = calculator.add(10, 50);
 		if (result!=60) {
-			System.out.println("Bad result: " + result);
+			//System.out.println("Bad result: " + result);
+			throw new IllegalStateException("Bad result: " + result);
 		}
 	}
 	
+	public static void main(String[] args) {
+		CalculatorTest test = new CalculatorTest();
+		try {
+			test.testAdd();
+		}catch (Throwable e) {
+			test.nbErrors ++;
+			e.printStackTrace();
+		}
+		if (test.nbErrors>0) {
+			throw new IllegalStateException("There are " + test.nbErrors + "error(s)");
+		}
+	}
 
 }
