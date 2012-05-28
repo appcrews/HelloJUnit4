caa74743fd15358d3f36c1a450603ada78f0fb01
f393907 <f393907@gmail.com>
2012-05-29 00:56:52 +0800

    TestDefaultController。testAddHandler, now code3.6, page32

--- src/test/java/com/appcrews/junit/ch03/TestDefaultController.java
+++ src/test/java/com/appcrews/junit/ch03/TestDefaultController.java
@@ -1,18 +1,48 @@
 package com.appcrews.junit.ch03;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class TestDefaultController {
 	private DefaultController controller;
+
+	private class SampleRequest implements Request {
+
+		@Override
+		public String getName() {
+			return "Test";
+		}
+		
+	}
 	
+	private class SampleHandler implements RequestHandler {
+
+		@Override
+		public Response process(Request request) throws Exception {
+			return new SampleResponse();
+		}
+		
+	}
+	
+	private class SampleResponse implements Response {
+		//empty
+	}
+	 
+
 	@Before
 	public void instantiate() throws Exception {
 		controller = new DefaultController();
 	}
 	
 	@Test 
-	public void testMethod() {
-		throw new RuntimeException("implement me");
+	public void testAddHandler() {
+		Request request = new SampleRequest();
+		RequestHandler handler = new SampleHandler();
+		controller.addHandler(request, handler);
+		RequestHandler handler2 = controller.getHandler(request);
+		assertSame("Handler we set in controller should be the same handler we get", handler, handler2);
 	}
 }
