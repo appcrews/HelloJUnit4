824f39f6af1bd3b3fc82043bb71b8ea1f1bcf64a
f393907 <f393907@gmail.com>
2012-05-29 00:00:51 +0800

    使用JUnit编写的CalculatorTest程序，此时：code1.4，page11

--- src/test/java/com/appcrews/junit/ch01/CalculatorTest.java
+++ src/test/java/com/appcrews/junit/ch01/CalculatorTest.java
@@ -1,28 +1,29 @@
 package com.appcrews.junit.ch01;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class CalculatorTest {
-	private int nbErrors = 0;
-	
+
+	@Test
 	public void testAdd() {
 		Calculator calculator = new Calculator();
 		double result = calculator.add(10, 50);
-		if (result!=60) {
-			//System.out.println("Bad result: " + result);
-			throw new IllegalStateException("Bad result: " + result);
-		}
+		assertEquals(60, result, 0);
 	}
 	
-	public static void main(String[] args) {
-		CalculatorTest test = new CalculatorTest();
-		try {
-			test.testAdd();
-		}catch (Throwable e) {
-			test.nbErrors ++;
-			e.printStackTrace();
-		}
-		if (test.nbErrors>0) {
-			throw new IllegalStateException("There are " + test.nbErrors + "error(s)");
-		}
-	}
+//	public static void main(String[] args) {
+//		CalculatorTest test = new CalculatorTest();
+//		try {
+//			test.testAdd();
+//		}catch (Throwable e) {
+//			test.nbErrors ++;
+//			e.printStackTrace();
+//		}
+//		if (test.nbErrors>0) {
+//			throw new IllegalStateException("There are " + test.nbErrors + "error(s)");
+//		}
+//	}
 
 }
